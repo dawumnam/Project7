49d6d80e90d79276043dc92aaaadab402dd6f593
Dawum Nam <dawumnam@gmail.com>
2021-04-30 15:54:11 -0800

    Initial Commit

---
+++ Project7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by dawum nam on 4/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
